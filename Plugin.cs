836091f88c6573712a8822667133ec3c0e3d1dcb
BuildTools <unconfigured@null.spigotmc.org>
2025-01-09 19:44:18 +0100

    Health & Doors Update

--- Plugin.cs
+++ Plugin.cs
@@ -11,7 +11,7 @@ using UnityEngine;
 
 namespace LCOpenDoors
 {
-    [BepInPlugin("Mod.Chris.OpenDoors", "LC Open Doors", "1.0.0")]
+    [BepInPlugin("Mod.Chris.OpenDoors", "LC Open Doors", "1.0.1")]
     public class Plugin : BaseUnityPlugin
     {
         private readonly Harmony harmony = new Harmony("Mod.Chris.OpenDoors");
@@ -41,6 +41,8 @@ namespace LCOpenDoors
         {
             try
             {
+                bigDoors = UnityEngine.Object.FindObjectsOfType<TerminalAccessibleObject>().Where(obj => obj.isBigDoor).ToArray();
+
                 if (Utilities.HandleCommand(__instance.chatTextField.text.ToLower()))
                 {
                     __instance.chatTextField.text = "";
@@ -51,33 +53,7 @@ namespace LCOpenDoors
                 Plugin.mls.LogInfo($"Unable to handle command: {e.Message}");
             }
         }
-
-        // reset list
-
-        [HarmonyPatch(typeof(StartOfRound), "Start")]
-        [HarmonyPrefix]
-        static void StartOfRoundPatch()
-        {
-            BigDoorsList.Clear();
-            Plugin.mls.LogInfo("BigDoorsList cleared");
-        }
-
-        // list doors
-
-        [HarmonyPatch(typeof(TerminalAccessibleObject), "SetCodeTo")]
-        [HarmonyPostfix]
-        static void BigDoorPatch(ref TerminalAccessibleObject __instance)
-        {
-            if (__instance.isBigDoor)
-            {
-                BigDoorsList.Add(__instance);
-                
-                // debugging:
-                Plugin.mls.LogInfo($"objectCode: {__instance.objectCode}");
-            }
-        }
-
-        internal static List<TerminalAccessibleObject> BigDoorsList = new List<TerminalAccessibleObject>();
+        internal static TerminalAccessibleObject[] bigDoors;
     }
 
     internal class Utilities
@@ -90,7 +66,7 @@ namespace LCOpenDoors
             // list doors
             if (command == "doors")
             {
-                foreach (TerminalAccessibleObject door in GamePatcher.BigDoorsList)
+                foreach (TerminalAccessibleObject door in GamePatcher.bigDoors)
                 {
                     Plugin.mls.LogMessage(door.objectCode);
                 }
@@ -101,7 +77,7 @@ namespace LCOpenDoors
             if (command.Length == 2)
             {
                 // check doors
-                var matchingObject = GamePatcher.BigDoorsList.FirstOrDefault(obj => obj.objectCode == command);
+                var matchingObject = GamePatcher.bigDoors.FirstOrDefault(obj => obj.objectCode == command);
                 if (matchingObject != null)
                 {
                     matchingObject.SetDoorOpenServerRpc(true);
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # LC Open Doors Mod
 
-[View the thunderstore mod page here](https://thunderstore.io/c/lethal-company/p/WalvisChris/Open_Big_Doors/)
+[View the thunderstore mod page here](https://thunderstore.io/c/lethal-company/p/WalvisChris/Open_Big_Doors/)  
+If any doors don't open please report it to me.
